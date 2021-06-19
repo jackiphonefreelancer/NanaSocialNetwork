f6e3cebb9c9d8f12b50f16076fc98e83c19e4fa7
Teerapat Champati <teerapat@7peakssoftware.com>
2021-06-20 06:28:26 +0700

    add app icon and firebase configuration file

--- NanaSocialNetwork/Screens/Onboarding/LogInViewController.swift
+++ NanaSocialNetwork/Screens/Onboarding/LogInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LogInViewController.swift
 //  NanaSocialNetwork
 //
 //  Created by Teerapat on 6/19/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
